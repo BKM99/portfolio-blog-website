3065a9843122af6fbf037cb2adfbde02f8b6a0cb
Brandon Morimoto <bmorimoto99@gmail.com>
2022-07-25 01:27:40 -0700

    more changes to project page

--- pages/projects.js
+++ pages/projects.js
@@ -13,9 +13,9 @@ const projects = () => {
             <div className='max-w-5xl md:px-4 py-8 pb-32 mx-auto space-y-28'>
 
                 <section className='p-4 md:p-8 bg-gray-100 rounded-lg shadow-2xl'>
-                    <h1 className='font-bold text-sm md:text-xl text-blue-500 pb-4'>Hand-written Equation Solver</h1>
+                    <h1 className='font-bold text-lg md:text-xl text-blue-500 pb-4'>Hand-written Equation Solver</h1>
                     <div className='grid grid-cols-1 gap-0 sm:gap-8 md:grid-cols-3 items-center'>
-                        <div className='aspect-w-3 aspect-h-2'>
+                        <div className='aspect-w-5 aspect-h-3'>
                             <Image src='/images/project-images/hwes-example.png' layout='fill' objectFit='contain' alt=''/>
                         </div>
                         <blockquote className='sm:col-span-2'>
@@ -31,7 +31,7 @@ const projects = () => {
                             </cite>
                             <div className='flex pt-8 space-x-4'>
                                 <div>
-                                    <a href='https://github.com/BKM99/MazeMania' target='_blank' rel='noopner noreferrer'>
+                                    <a href='https://github.com/BKM99/hand-written-equation-solver' target='_blank' rel='noopner noreferrer'>
                                         <FiGithub size='30px'/>
                                     </a>
                                 </div>
@@ -44,25 +44,25 @@ const projects = () => {
                 </section>
 
                 <section className='p-4 md:p-8 bg-gray-100 rounded-lg shadow-2xl'>
-                    <h1 className='font-bold text-sm md:text-xl text-blue-500 pb-4'>Malware Classification via Ensemble Learning</h1>
+                    <h1 className='font-bold text-lg md:text-xl text-blue-500 pb-4'>Malware Classification via Ensemble Learning</h1>
                     <div className='grid grid-cols-1 gap-0 sm:gap-8 md:grid-cols-3 items-center'>
-                        <div className='aspect-w-3 aspect-h-2'>
+                        <div className='aspect-w-4 aspect-h-3'>
                             <Image src='/images/project-images/stacked-roc.png' layout='fill' objectFit='contain' alt=''/>
                         </div>
                         <blockquote className='sm:col-span-2'>
                             <p className='text-xs md:text-base'>
-                                Developed a web application that uses a convolutional neural network (VGG16) to solve math equations. Designed the UI with React and the
-                                backend with Django. Built and trained the model with Tensorflow and processed the image dataset with OpenCV.
+                                Stacked Hidden Markov Models (HMM) and Support Vector Machines (SVM) to classify malware samples based on opcode sequences. Implemented the HMM
+                                from scratch using Java and used sklearn’s SVM implementation to get an AUC of 85%-100% depending on the malware family.
                             </p>
                             <cite className='inline-flex items-center mt-8 not-italic'>
                                 <span className='hidden w-6 h-px bg-gray-400 sm:inline-block'></span>
                                 <p className='text-xs md:text-base text-gray-500 sm:ml-3'>
-                                    Python, Javascript, Tensorflow, Django, React, OpenCV
+                                    Java, Python, Scikit-Learn, Pandas, Matplotlib
                                 </p>
                             </cite>
                             <div className='flex pt-8 space-x-4'>
                                 <div>
-                                    <a href='https://github.com/BKM99/MazeMania' target='_blank' rel='noopner noreferrer'>
+                                    <a href='https://github.com/BKM99/Malware-Classification-with-HMM-and-SVM' target='_blank' rel='noopner noreferrer'>
                                         <FiGithub size='30px'/>
                                     </a>
                                 </div>
@@ -75,20 +75,20 @@ const projects = () => {
                 </section>
 
                 <section className='p-4 md:p-8 bg-gray-100 rounded-lg shadow-2xl'>
-                    <h1 className='font-bold text-sm md:text-xl text-blue-500 pb-4'>Maze Mania</h1>
+                    <h1 className='font-bold text-lg md:text-xl text-blue-500 pb-4'>Maze Mania</h1>
                     <div className='grid grid-cols-1 gap-0 sm:gap-8 md:grid-cols-3 items-center'>
-                        <div className='aspect-w-3 aspect-h-2'>
+                        <div className='aspect-w-1 aspect-h-1'>
                             <Image src='/images/project-images/maze-mania-ingame.png' layout='fill' objectFit='contain' alt=''/>
                         </div>
                         <blockquote className='sm:col-span-2'>
                             <p className='text-xs md:text-base'>
-                                Developed a web application that uses a convolutional neural network (VGG16) to solve math equations. Designed the UI with React and the
-                                backend with Django. Built and trained the model with Tensorflow and processed the image dataset with OpenCV.
+                                Built a maze game where the user moves a dog carrying a pizza through a series of mazes. Game features include in-game notifications, reset/time traps,
+                                and increases in difficulty throughout the mazes. The project was structured using the MVC design pattern and tested using JUnit.
                             </p>
                             <cite className='inline-flex items-center mt-8 not-italic'>
                                 <span className='hidden w-6 h-px bg-gray-400 sm:inline-block'></span>
                                 <p className='text-xs md:text-base text-gray-500 sm:ml-3'>
-                                    Python, Javascript, Tensorflow, Django, React, OpenCV
+                                    Java, Java Swing, JUnit
                                 </p>
                             </cite>
                             <div className='flex pt-8 space-x-4'>
@@ -106,25 +106,25 @@ const projects = () => {
                 </section>
 
                 <section className='p-4 md:p-8 bg-gray-100 rounded-lg shadow-2xl'>
-                    <h1 className='font-bold text-sm md:text-xl text-blue-500 pb-4'>Personal Portfolio and Blog</h1>
+                    <h1 className='font-bold text-lg md:text-xl text-blue-500 pb-4'>Personal Portfolio and Blog</h1>
                     <div className='grid grid-cols-1 gap-0 sm:gap-8 md:grid-cols-3 items-center'>
-                        <div className='aspect-w-3 aspect-h-2'>
+                        <div className='aspect-w-5 aspect-h-3'>
                             <Image src='/images/project-images/home-page-portfolio.png' layout='fill' objectFit='contain' alt=''/>
                         </div>
                         <blockquote className='sm:col-span-2'>
                             <p className='text-xs md:text-base'>
-                                Developed a web application that uses a convolutional neural network (VGG16) to solve math equations. Designed the UI with React and the
-                                backend with Django. Built and trained the model with Tensorflow and processed the image dataset with OpenCV.
+                                Designed and built my own portfolio and blog. Nothing much to say about it here
+                                since you are already on the website.
                             </p>
                             <cite className='inline-flex items-center mt-8 not-italic'>
                                 <span className='hidden w-6 h-px bg-gray-400 sm:inline-block'></span>
                                 <p className='text-xs md:text-base text-gray-500 sm:ml-3'>
-                                    Python, Javascript, Tensorflow, Django, React, OpenCV
+                                    Next.js, Tailwind CSS, framer-motion
                                 </p>
                             </cite>
                             <div className='flex pt-8 space-x-4'>
                                 <div>
-                                    <a href='https://github.com/BKM99/MazeMania' target='_blank' rel='noopner noreferrer'>
+                                    <a href='https://github.com/BKM99/portfolio-blog-website' target='_blank' rel='noopner noreferrer'>
                                         <FiGithub size='30px'/>
                                     </a>
                                 </div>
